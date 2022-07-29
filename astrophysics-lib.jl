a1e00015920506c0417348f370d633c1eb361077
Kijan Cassel <109874410+KijanC@users.noreply.github.com>
2022-07-29 12:54:12 +0200

    Update astrophysics-lib.jl

--- astrophysics-lib.jl
+++ astrophysics-lib.jl
@@ -1,1 +1,9 @@
-
+function F(m1, m2, r)
+  # gravitational force between to objects
+  # F = ((m1 * m2) / r**2) * G
+  
+  G = 6,67 * 10 ^ -11
+  (F = m1 * m2; F = F / r ^ 2; F = F * G)
+  
+  return F;
+end
